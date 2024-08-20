a2330a9b9f5ce6b235de38ca59a7b42dfee44e79
Marc-Antoine Arnaud <marc-antoine.arnaud@luminvent.com>
2024-08-20 10:24:39 +0200

    feat: add sea-orm to create User table

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+mod user;
+
+use axum::extract::State;
 use axum::{
     http::StatusCode,
     routing::{get, post},
     Json, Router,
 };
 use clap::Parser;
+use migration::{Migrator, MigratorTrait};
+use sea_orm::ActiveValue::Set;
+use sea_orm::{ActiveModelTrait, Database, DatabaseConnection, EntityTrait};
 use serde::{Deserialize, Serialize};
 use tower_http::trace::TraceLayer;
 
@@ -13,6 +19,9 @@ struct Args {
     /// Port to serve
     #[arg(short, long, default_value_t = 4000)]
     port: u16,
+    /// URL to the database
+    #[arg(short, long, default_value = "sqlite::memory:")]
+    database_url: String,
 }
 
 #[tokio::main]
@@ -35,11 +44,18 @@ async fn main() -> std::io::Result<()> {
         .apply()
         .unwrap();
 
+    let database_connection = Database::connect(args.database_url)
+        .await
+        .expect("Database connection failed");
+
+    Migrator::up(&database_connection, None).await.unwrap();
+
     let app = Router::new()
         .route("/", get(root))
         .route("/users", get(list_users))
         .route("/users", post(create_user))
-        .layer(TraceLayer::new_for_http());
+        .layer(TraceLayer::new_for_http())
+        .with_state(database_connection);
 
     let bind_address = format!("0.0.0.0:{}", args.port);
 
@@ -54,14 +70,33 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn list_users() -> (StatusCode, Json<Vec<User>>) {
-    (StatusCode::CREATED, Json(Vec::<User>::new()))
+async fn list_users(state: State<DatabaseConnection>) -> (StatusCode, Json<Vec<User>>) {
+    let users: Vec<user::Model> = user::Entity::find().all(&state.0).await.unwrap();
+
+    let users = users
+        .into_iter()
+        .map(|model_user| User {
+            id: model_user.id as u64,
+            username: model_user.username,
+        })
+        .collect();
+
+    (StatusCode::CREATED, Json(users))
 }
 
-async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+async fn create_user(
+    state: State<DatabaseConnection>,
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    let model_user = user::ActiveModel {
+        username: Set(payload.username.to_string()),
+        ..Default::default()
+    };
+    let model_user = model_user.insert(&state.0).await.unwrap();
+
     let user = User {
-        id: 1337,
-        username: payload.username,
+        id: model_user.id as u64,
+        username: model_user.username,
     };
 
     (StatusCode::CREATED, Json(user))
